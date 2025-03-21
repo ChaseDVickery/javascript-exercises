ffa4543d979ba39b246778b7ad83b112488d4648
ChaseDVickery <chasedvickery@gmail.com>
2025-03-20 22:01:26 -0400

    script02 practice complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reverseInput) {
+    let reversedString = "";
+    for (let i = reverseInput.length-1; i >= 0; i--) {
+        reversedString += reverseInput.at(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
