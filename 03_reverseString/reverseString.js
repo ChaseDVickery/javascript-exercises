2209388ce9b2fe8ddbf623abfaa43a014d3b1793
ChaseDVickery <chasedvickery@gmail.com>
2025-03-20 22:03:14 -0400

    script03 practice revised

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -6,5 +6,10 @@ const reverseString = function(reverseInput) {
     return reversedString;
 };
 
+// string.split("") returns a split array of character strings that can be reversed with array.reverse(), which can then be joined back with array.join():
+// const reverseString = function (string) {
+//     return string.split("").reverse().join("");
+// };
+
 // Do not edit below this line
 module.exports = reverseString;
