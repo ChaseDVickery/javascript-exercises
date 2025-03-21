984b1b40c74eb3c0c6886a8478fc0585e6e9c820
ChaseDVickery <chasedvickery@gmail.com>
2025-03-21 18:00:14 -0400

    script08 practice complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	return values.reduce((total, val) => total+val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  return values.reduce((total, val) => total * val);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for (let i = a; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
