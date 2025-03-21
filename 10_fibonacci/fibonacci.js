9647a785dff0600f5a66f9533bacb0e1d97bd273
ChaseDVickery <chasedvickery@gmail.com>
2025-03-21 19:39:52 -0400

    script10 practice complete using array stuff

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(iterations) {
+    iterations = parseInt(iterations);
+    if (iterations === 0) { return 0; }
+    else if (iterations < 0) { return "OOPS"; }
+    const starter = [0, 1];
+    // We could do this recursively, but
+    // I don't know how that would play
+    // with the testing system
+    for (let i = 0; i < iterations-1; i++) {
 
+        starter.splice(0, 2, starter[1], starter[0] + starter[1]);
+    }
+    return starter[1];
 };
 
 // Do not edit below this line
