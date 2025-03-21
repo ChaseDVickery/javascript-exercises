cb65d2c9e36b231bd377ae3080c12eabbb13fa47
ChaseDVickery <chasedvickery@gmail.com>
2025-03-20 22:40:22 -0400

    script05 practice complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstValue, secondValue) {
+    if (
+        typeof(firstValue) !== "number" ||
+        !Number.isInteger(firstValue) ||
+        firstValue < 0 ||
+        typeof(secondValue) !== "number" ||
+        !Number.isInteger(secondValue) ||
+        secondValue < 0
+    ) {
+        return "ERROR";
+    }
 
+    let low     = Math.min(firstValue, secondValue);
+    let high    = Math.max(firstValue, secondValue);
+    let sum     = 0;
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
