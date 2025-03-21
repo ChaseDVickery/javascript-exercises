1b5fccfb665bcb171fd4d32341c6b819904f7689
ChaseDVickery <chasedvickery@gmail.com>
2025-03-20 22:30:56 -0400

    script04 practice with 3 methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayValues, ...removalValues) {
+    // // Method 1: Iterate removal manually
+    // for (const toRemove of removalValues) {
+    //     // Iterate backwards to not conflict indices
+    //     for (let i = arrayValues.length-1; i >= 0; i--) {
+    //         if (arrayValues[i] === toRemove) {
+    //             arrayValues.splice(i, 1);
+    //         }
+    //     }
+    // }
+    // return arrayValues;
+
+    // // Method 2: Filter?
+    // return arrayValues.filter((value) => {
+    //     for (const toRemove of removalValues) {
+    //         if (value === toRemove) { return false; }
+    //     }
+    //     return true;
+    // });
+
+    // Method 3: Filter++
+    return arrayValues.filter((value) => {
+        return !removalValues.includes(value);
+    });
 };
 
 // Do not edit below this line
