6c2cfdf4c58fda84b647c63de45753b7133175f3
ChaseDVickery <chasedvickery@gmail.com>
2025-03-21 20:10:06 -0400

    script12 practice complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldDeath = oldest.yearOfDeath ? oldest.yearOfDeath : new Date().getFullYear();
+        let currDeath = current.yearOfDeath ? current.yearOfDeath : new Date().getFullYear();
+        if ((currDeath - current.yearOfBirth) > (oldDeath - oldest.yearOfBirth)) {
+            return current;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
